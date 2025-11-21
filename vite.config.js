2928565e8a9aba4984638c6696b2c26fc0aa551e
Mark <wikimark2002gmaol.com>
2025-11-20 23:46:51 -0400

    fix: Configure Vercel for SPA and fix MIME type error

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  server: {
+    port: 3000,
+  },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  },
+  base: './',
 })
